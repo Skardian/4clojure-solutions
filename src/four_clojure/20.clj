76cf57626ce8ea9aa6a04ee50d6a6a75f3bfd402
Pepe Padial <jose.carlos.padial@gmail.com>
2017-08-25 19:05:07 +0000

    Fix problem 20 solution

--- src/four_clojure/20.clj
+++ src/four_clojure/20.clj
@@ -1,4 +1,4 @@
 (= (#(first (rest (reverse %))) (list 1 2 3 4 5)) 4)
-(= (__ ["a" "b" "c"]) "b")
-(= (__ [[1 2] [3 4]]) [1 2])
+(= (#(first (rest (reverse %))) ["a" "b" "c"]) "b")
+(= (#(first (rest (reverse %))) [[1 2] [3 4]]) [1 2])
 
