25c06574feda46478699e68e31662e5c6b32f4f0
Pepe Padial <jose.carlos.padial@gmail.com>
2017-08-31 18:19:35 +0000

    Revisit problems 41-50

--- src/four_clojure/42.clj
+++ src/four_clojure/42.clj
@@ -3,11 +3,11 @@
   (
    ;; Begin code
    (fn factorial [x]
-     (reduce * (map inc (range x))))
+     (reduce * (range 1 (inc x))))
    ;; End code
    x))
 (__ 1)
-	
+
 (= (__ 1) 1)
 (= (__ 3) 6)
 (= (__ 5) 120)
--- src/four_clojure/46.clj
+++ src/four_clojure/46.clj
@@ -2,14 +2,15 @@
   [x]
   (
    ;; Begin code
-   (fn flip-args [f]
+   (fn flip-args
+     [f]
      (fn [& xs]
        (apply f (reverse xs))))
    ;; End code
    x))
 
 (= 3 ((__ nth) 2 [1 2 3 4 5]))
-	
+
 (= 3 ((__ nth) 2 [1 2 3 4 5]))
 (= true ((__ >) 7 8))
 (= 4 ((__ quot) 2 8))
